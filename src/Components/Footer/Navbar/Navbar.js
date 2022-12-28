91fe957a656f5de562f83b99ac2fbc3a3f554f3a
dev-alamin1 <dev.alaminmondal@gmail.com>
2022-12-28 20:32:42 +0600

    first commit

---
+++ src/Components/Footer/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+
+// import { useState, useEffect } from "react";
+// import {
+//   Navbar,
+//   MobileNav,
+//   Typography,
+//   Button,
+//   IconButton,
+// } from "@material-tailwind/react";
+
+// const Navbar = () => {
+//     return (
+//         <div>
+            
+//         </div>
+//     );
+// };
+
+// export default Navbar;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6nI7ByTkHD_pqsao8V3b-crLBKOiW-Sc",
+  authDomain: "to-do-task-739a9.firebaseapp.com",
+  projectId: "to-do-task-739a9",
+  storageBucket: "to-do-task-739a9.appspot.com",
+  messagingSenderId: "889059364959",
+  appId: "1:889059364959:web:69552bfc7580db0408cbea"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
