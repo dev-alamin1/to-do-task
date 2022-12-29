034073dac12dbebe118752df36133634b1a5dcf2
dev-alamin1 <dev.alaminmondal@gmail.com>
2022-12-29 09:27:55 +0600

    taskname add from homepage done

--- src/Components/Cards/HomePageTaskCard.js
+++ src/Components/Cards/HomePageTaskCard.js
@@ -9,12 +9,35 @@ import {
   Input,
   Checkbox,
 } from "@material-tailwind/react";
+import { toast } from "react-hot-toast";
 
 const HomePageTaskCard = () => {
 
     const submitHanlder = (e)=>{
             e.preventDefault();
             console.log(e.target.taskname.value)
+            const form = e.target;
+            const taskName = form.taskname.value;
+
+            const taskInfo ={
+              taskName
+            }
+
+            fetch('http://localhost:5000/tasks',{
+              method:'POST',
+              headers:{
+                'content-type':'application/json'
+              },
+              body: JSON.stringify(taskInfo)
+            })
+            .then(res=>res.json())
+            .then(data=>{
+              if(data.acknowledged)
+              {
+                toast.success('task added successfully')
+                form.reset();
+              }
+            })
     }
   return (
     <Card className="w-96 mt-10">
